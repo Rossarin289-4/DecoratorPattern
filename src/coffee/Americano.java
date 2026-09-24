b45c6d51c16a5ba21c89c1cfa72799d6a1d88e04
Rossarin289-4 <rossarin.m@kkumail.com>
2026-09-24 12:57:58 +0700

    Implement Decorator Pattern for Coffee

---
+++ src/coffee/Americano.java
@@ -0,0 +1,13 @@
+package coffee;
+
+public class Americano implements Coffee{
+    @Override
+    public String getDescription() {
+        return "Americano";
+    }
+
+    @Override
+    public double getCost() {
+        return 2.50;
+    }
+}
